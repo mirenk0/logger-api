1d91c45fe9bafa6b7bf4de19914885b8c6494016
mirenk0 <70020530+mirenk0@users.noreply.github.com>
2024-07-10 10:56:38 +0200

    Delete dist directory

--- dist/config/local.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    port: 3000
-};
-//# sourceMappingURL=local.js.map
--- dist/config/prod.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    port: 5000
-};
-//# sourceMappingURL=prod.js.map
